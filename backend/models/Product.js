91632589149b6533e03c7a24a28840cdc8993c16
franco <francobg7@gmail.com>
2025-02-01 21:23:11 -0300

    agregando stock

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,6 +4,7 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String },
+    stock: { type: Number, required: true, default: 0 }
 });
 
 export default mongoose.model('Product', productSchema);
