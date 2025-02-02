e3742ecb6bcb21b9f14b364e23cc048bcdbb3bfa
franco <francobg7@gmail.com>
2025-02-02 14:23:08 -0300

    cambios en el Schema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    products: [{
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-        quantity: { type: Number, required: true }
-    }],
-    date: { type: Date, default: Date.now },
-});
+const orderSchema = new mongoose.Schema({     
+    name: { type: String, required: true },     
+    address: { type: String, required: true },     
+    products: { type: Array, required: true },     
+    date: { type: Date, default: Date.now }, 
+});  
 
-export default mongoose.model("Order", orderSchema);
+export default mongoose.model("Order", orderSchema); 
