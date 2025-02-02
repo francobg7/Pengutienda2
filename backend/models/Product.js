67230c37a26d7c0090087d46b266cfb1174268fa
franco <francobg7@gmail.com>
2025-02-02 14:23:41 -0300

    cambios en el Schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,7 +4,6 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String },
-    stock: { type: Number, required: true, default: 0 }
 });
 
 export default mongoose.model('Product', productSchema);
